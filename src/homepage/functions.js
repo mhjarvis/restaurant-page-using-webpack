c67157c39387e4b85c67b65ed19344cb30dc5187
Markus <markus.h.jarvis@gmail.com>
2022-09-14 09:37:23 -0500

    add function for updating innerHTML

--- src/homepage/functions.js
+++ src/homepage/functions.js
@@ -1,6 +1,6 @@
-function createElement(tag, elementToCreate, id) {
+function createElement(tagToGet, elementToCreate, id) {
 
-    const getElement = document.querySelector(tag);
+    const getElement = document.querySelector(tagToGet);
     const element = document.createElement(elementToCreate);
     element.id = id;
 
@@ -8,4 +8,8 @@ function createElement(tag, elementToCreate, id) {
 
 }
 
-export { createElement }
+function addInnerHTML(tagToGet, contentToAdd) {
+    const getElement = document.querySelector(tagToGet);
+    getElement.innerHTML = contentToAdd;
+}
+export { createElement, addInnerHTML }
