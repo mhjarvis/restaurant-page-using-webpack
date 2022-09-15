6e4c9a7a11ec087d256b22c5eb501faa192799fc
Markus <markus.h.jarvis@gmail.com>
2022-09-15 00:01:33 -0500

    create div to house social-media links

--- src/homepage/createNavigation.js
+++ src/homepage/createNavigation.js
@@ -2,25 +2,12 @@ import { addInnerHTML, createElement } from "./functions";
 
 const createNavigation = function() {
 
-/*  
-    const content = document.querySelector("#content");
-    const header = document.createElement("header");
-    content.appendChild(header); 
-    
-    const getElement = document.querySelector(tag);
-    const element = document.createElement(elementToCreate);
-    element.id = id;
-
-    getElement.appendChild(element);
-*/
-
     // create Header element
     createElement('#content', 'header', 'header');
 
     // create Logo element
     createElement('header', 'span', 'brand');
     addInnerHTML('#brand', 'The Kraken');
-
 
     // create Navigation bar with link tags
     createElement('header', 'nav', 'navigation');
@@ -35,11 +22,13 @@ const createNavigation = function() {
     addInnerHTML('#about', 'About');
 
     // create facebook/instagram buttons/links
-    createElement('header', 'img', 'facebook-link');
+    createElement('header', 'div', 'social-links')
+
+    createElement('#social-links', 'img', 'facebook-link');
     const addFacebookImage = document.querySelector('#facebook-link');
     addFacebookImage.setAttribute('src', './images/facebook.png');
 
-    createElement('header', 'img', 'instagram-link');
+    createElement('#social-links', 'img', 'instagram-link');
     const addInstagramImage = document.querySelector('#instagram-link');
     addInstagramImage.setAttribute('src', './images/instagram.png');
 }
