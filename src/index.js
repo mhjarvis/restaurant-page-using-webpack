c574f66148b8b92c5f4bc1ef18d97cd539c6cedb
Markus <markus.h.jarvis@gmail.com>
2022-09-20 16:18:29 -0500

    add createFooter() to index

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
+import { createFooter } from "./homepage/createFooter.js";
 import { createHeader } from "./homepage/createHeader.js";
 import { createMain } from "./homepage/createMain.js";
 
 createHeader();
-
 createMain();
+createFooter();
 
 
 console.log("hello")
