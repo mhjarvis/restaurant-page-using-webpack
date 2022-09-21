edbd73a60b68cf0851c5aa887bcd6296b30143b8
Markus <markus.h.jarvis@gmail.com>
2022-09-21 00:01:48 -0500

    Fix issue where eventListeners were not working

--- src/index.js
+++ src/index.js
@@ -6,4 +6,44 @@ import { createMenu } from "./menu/createMenu"
 createHeader();
 createMain();
 createFooter();
+addListeners();
 
+
+function addListeners() {
+
+const home = document.getElementById('home');
+    home.addEventListener('click', function() {
+    resetContent();
+    createHeader();
+    createMain();
+    createFooter();
+    addListeners();
+})
+
+const menu = document.getElementById('menu');
+    menu.addEventListener('click', function() {
+    resetContent();
+    createHeader();
+    createMenu();
+    createFooter();
+    addListeners();
+
+})
+
+const contact = document.getElementById('contact');
+    contact.addEventListener('click', function() {
+    resetContent();
+    createHeader();
+    createMain();
+//    creteContact();
+    createFooter();
+    addListeners();
+})
+}
+
+
+
+function resetContent() {
+    const content = document.getElementById('content');
+    content.innerHTML = '';
+}
