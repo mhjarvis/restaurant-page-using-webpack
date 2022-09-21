3e553c95e9dfd8c9a75ff23787801b9153857fe1
Markus <markus.h.jarvis@gmail.com>
2022-09-20 23:41:40 -0500

    reload old files; npm errors gone

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import { createFooter } from "./homepage/createFooter";
+import { createHeader } from "./homepage/createHeader";
+import { createMain } from "./homepage/createMain"
+import { createMenu } from "./menu/createMenu"
+
+createHeader();
+createMain();
+createFooter();
+
