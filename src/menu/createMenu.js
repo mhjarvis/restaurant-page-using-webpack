6007618914a1fc4edb217db7ac04940d25a52b85
Markus <markus.h.jarvis@gmail.com>
2022-09-22 07:46:56 -0500

    update images absolute path

--- src/menu/createMenu.js
+++ src/menu/createMenu.js
@@ -32,7 +32,7 @@ let createMenu = () => {
     /* Create section right */
     createElement('#main', 'section', 'right');
     createElement('#right', 'img', 'kraken-img');
-    document.querySelector('#kraken-img').src = '../dist/images/kraken.png';
+    document.querySelector('#kraken-img').src = 'https://github.com/mhjarvis/restaurant-page-using-webpack/blob/main/dist/images/kraken.png?raw=true';
 }
 
 
